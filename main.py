c5745f4926265946c2f9955967626aeabaad6a78
vloziak <150846733+vloziak@users.noreply.github.com>
2024-06-15 10:48:02 +0300

    added some changes

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import numpy as np
 
-def find_eigenvalues(matrix):
-    eigenvalues = np.linalg.eigvals(matrix)
-    return eigenvalues
+def find_eigenvectors_eigenvalues(matrix):
+    eigenvalues, eigenvectors = np.linalg.eig(matrix)
+    return eigenvalues, eigenvectors
+def checking_eigenvectors(matrix, eigenvalues, eigenvectors):
+    print("Перевірка рівності A⋅v = λ⋅v:")
 
-def find_eigenvectors(matrix):
-    eigenvectors = np.linalg.eig(matrix)
-    return eigenvectors
+    for i in range(len(eigenvalues)):
+        eigenvalue = eigenvalues[i]
+        eigenvector = eigenvectors[:, i]
+        A_v = np.dot(matrix, eigenvector)
+        λ_v = eigenvalue * eigenvector
+
+        print(f"\nВласне значення λ = {eigenvalue}")
+        print(f"Власний вектор v = {eigenvector}")
+        print(f"A⋅v = {A_v}")
+        print(f"λ⋅v = {λ_v}")
+        
+        if np.allclose(A_v, λ_v):
+            print(f"Рівність A⋅v = λ⋅v виконується для λ = {eigenvalue}\n")
+        else:
+            print(f"Рівність A⋅v = λ⋅v НЕ виконується для λ = {eigenvalue}\n")
+
 
 A = np.array([[-5, 0, 3],
               [-6, 1, 3],
               [-6, 0, 4]])
 
-eigenvalues = find_eigenvalues(A)
-eigenvectors = find_eigenvectors(A)
+eigenvalues,eigenvectors = find_eigenvectors_eigenvalues(A)
 
 print("Власні значення:", eigenvalues)
 print("Власні вектори:", eigenvectors)
+checking_eigenvectors(A, eigenvalues, eigenvectors)
